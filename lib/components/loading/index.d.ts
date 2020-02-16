9e71adab8b77738291aadf59642eeaf4a68ddeb0
Alain <yinxulai@hotmail.com>
2020-02-17 00:33:25 +0800

    移除线上 lib 目录

--- lib/components/loading/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { IProps as BaseProps } from '../container';
-export interface IProps extends BaseProps {
-    lowest?: number;
-    loading?: boolean;
-}
-export declare const Loading: React.FC<React.PropsWithChildren<IProps>>;
-export default Loading;
-//# sourceMappingURL=index.d.ts.map
--- lib/helper/listener/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type Subscriber = () => any;
-export declare class Listener {
-    subscribers: Map<Symbol, Subscriber>;
-    addListener(subscriber: Subscriber): () => boolean;
-    removeListener(symbol: Symbol): boolean;
-    dispatchSubscribers(): Promise<void>;
-}
-//# sourceMappingURL=index.d.ts.map
